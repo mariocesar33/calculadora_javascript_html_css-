949729c2ce64ad7377bb8cfdee3b47fbba347a75
Mário César Silva <mcgato33@hotmail.com>
2020-08-19 15:55:45 -0100

    projet calc

---
+++ README.md
@@ -0,0 +1,4 @@
+# Calculadora
+## calculadora 1.0 desenvolvido apenas com javascript, html e css.
+
+![interface](https://github.com/mariocesar33/calculadora_javascript_html_css-/blob/master/calculadora.png).
---
+++ script/controller/calcController.js
@@ -0,0 +1,226 @@
+class CalcController {
+
+    constructor() {
+        this._operation = [];
+        this._locale = 'pt-br';
+        this._displayCalcEl = document.querySelector('input[type="text"]');
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('input[type="time"]');
+        this._currentDate;
+
+        this.initialize();
+        this.initButtonsEvents();
+    }     
+    
+    initialize() {
+
+        this.setDisplayDateTime();
+
+        setInterval( ()=> {
+            this.setDisplayDateTime();
+        }, 1000);
+    }
+
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach(event=> {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    clearAll() {
+        this._operation = null;
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+    /** verificar o ultimo elemento */
+    getLastOperation() {
+        return this._operation[this._operation.length-1]; // pegar o ultimo elemento do arry
+    }
+
+    setLastOperation(value) {
+        this._operation[this._operation.length-1] = value; // passar um valor na posicao de uma outra que ja existia (o que ja existia deixa de existir, dando lugar a outra nova valor)
+                                                           // ex: coloca-> "3" proximo coloca "4" ele tira "3" e coloca-> "34"
+    }
+
+    isOperator(value) { // verificar se o value é um operador
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1) // indexOf() percore o array, se o valor do value for igual ao algum elemento que se
+                                                               // encontra dentro do array, vai retornar o index do elemento se não retorna (-1)
+    }
+
+    pushOperation(value) {//verificar se tem três elementos dentro de array (validacão)
+        this._operation.push(value); // só é responsavel para fazer o push
+
+        if(this._operation.length > 3) { // se (numero operador numero) -> se for colocar mais um operador, fazer a operacao
+                                         //  de (numero operador numero = resultado), então: resultado operador novoNumero 
+            this.calc();  
+            console.log(this._operation)
+        }
+    }
+
+    calc(){
+        let last = this._operation.pop(); // tirar e guardar a ultima operacao
+        // this._operation que tem toda a nossa operacao guardada. vamos calcula-lo
+
+        if (last == "%") { //verificar se ultimo é simbolo %
+
+        } else {
+            let result = eval(this._operation.join("")); // a funcao 'eval()' é quem faz as operacões
+            this._operation = [result, last]; // atribuir novos valores ao array this._operation
+        }
+        this.setLastNumberToDisplay(); // mostrar na tela a operacão feita
+    }
+
+    setLastNumberToDisplay() { // o metodo usado para mostrar o ultimo numero no display
+        let lastNumber;
+
+        for(let i = this._operation.length - 1; i >= 0; i--) {
+            if(!this.isOperator(this._operation[i])){ //perguntar se o que esta dentro do this._operation[i], se não é um "operador", se não é um operador então já encontrei um numero.
+            lastNumber = this._operation[i];// se o i não for um operador quer dizer que ja encotrei um numero, e vou colocar o numero na minha variavel
+            break; // tendo o numero na minha variavel, mando parar o for...
+            }
+        }
+        // para colocar esse ultimo numero na tela, é basta chamar o objecto this.displayCalc e colocar igual ao lasNumber
+        this.displayCalc = lastNumber;
+    }
+
+    addOperation(value) { // aqui eu mexo nos numeros (adicionando uma nova operacão)
+
+        if(isNaN(this.getLastOperation())) { //string ( + - / * % . )
+
+            if(this.isOperator(value)){
+                this.setLastOperation(value); // concatenar valores (ex: "2" "5" "6" -> 256)
+            } else if(isNaN(value)) {
+                console.log("outra coisa", value)
+            } else {
+                this.pushOperation(value); // fazer push
+                this.setLastNumberToDisplay(); // inserir o primeiro numero na tela
+            }
+            
+        } else {
+
+            if (this.isOperator(value)) { // o que entrou é operador??
+                this.pushOperation(value); //sim. adiciona no array, em uma outra posicão então (fazer push)
+            } else {
+                let newValue = this.getLastOperation().toString() + value.toString(); //number (concatenar-> "3 5 -> 35")  -> aqui mexo nos numeros: estou modificando o numero
+                this.setLastOperation(parseInt(newValue));// newValue-> vai ser o valor final,  ("6" "2" "4"   624 <--newValue)
+                
+                // Atualizar o dilplay
+                this.setLastNumberToDisplay(); // mostra o ultimo numero no display
+            }
+        }
+        console.log(this._operation)
+    }
+
+    setError() {
+        this.displayCalc = "ERROR"; //mensagem de erro
+    }
+
+    execBtn(value) {
+        switch(value) {
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case '+':
+                this.addOperation('+');
+                break;
+            case '-':
+                this.addOperation('-');
+                break;
+            case '/':
+                this.addOperation('/');
+                break;
+            case '*':
+                this.addOperation('*');
+                break;
+            case '%':
+                this.addOperation('%');
+                break;
+            case '.':
+                this.addOperation('.');
+                break;
+            case '=':
+
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+            break;
+        }
+
+    }
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll('.calculator-keys > button');
+        
+        buttons.forEach((btns, index) => {
+            this.addEventListenerAll(btns, "click drag", e => {
+                let textBtn = btns.value
+
+                this.execBtn(textBtn);
+            });
+
+            this.addEventListenerAll(btns, "mouseover mouseup mousedow", e =>{ 
+                btns.style.cursor = "pointer";// pointer -> colocar sinal de mão ao passar em cima de butões
+            });
+        });    
+    }
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, { //formatar data
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale); 
+    }
+
+    get displayTime(){
+        return this._timeEl.value;
+    }
+
+    set displayTime(value) {
+        return this._timeEl.value = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
+    }
+
+    get displayCalc() {
+        return this._displayCalcEl.value;
+    }
+
+    set displayCalc(value) {
+        this._displayCalcEl.value = value;
+    }
+
+    get currentDate() {
+        return new Date();
+    }
+
+    set currentDate(value) {
+        this._currentDate = value;
+    }
+    
+}
